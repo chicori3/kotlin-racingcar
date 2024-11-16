d64f5ffce4fc39e3e7d025075c592ab5b5c0f4c7
sian <chicori3@naver.com>
2024-11-16 17:43:03 +0900

    refactor: 자동차 생성 시 숫자 생성기를 받아 생성

--- src/main/kotlin/racing/CarFactory.kt
+++ src/main/kotlin/racing/CarFactory.kt
@@ -3,12 +3,20 @@ package racing
 object CarFactory {
     private var sequence: Int = 0
 
-    fun createCar(position: Int = 0): Car {
-        return Car.from(sequence++, position)
+    fun createCar(position: Int = 0, generator: CountGenerator): Car {
+        return Car.from(
+            sequence = sequence++,
+            position = position,
+            generator = generator,
+        )
     }
 
-    fun createCars(quantity: Int): List<Car> {
-        return (0 until quantity).map { createCar() }
+    fun createCars(quantity: Int, generator: CountGenerator): List<Car> {
+        return (0 until quantity).map {
+            createCar(
+                generator = generator,
+            )
+        }
     }
 
     fun resetSequence() {
